8cc1da4b853f7ff94c0bb661674e5c5b48518bf1
Oktikk <65051521+Oktikk@users.noreply.github.com>
2023-07-14 19:54:51 +0200

    Added bash

---
+++ .github/runtests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FAILURES=0
+TEST_PROJECTS=$(find test -name "NotificationSystemServer.*.Tests")
+
+for PROJECT in $TEST_PROJECTS
+do 
+    (dotnet test ./$PROJECT --no-build --verbosity normal)
+    if test "$?" != "0" 
+    then ((FAILURES+=1)) 
+    fi    
+done
+
+exit $FAILURES
